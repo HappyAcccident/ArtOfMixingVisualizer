cec716282fbe6621e1bc338ad13945f01d5735cc
HappyAcccident <hppyaccdnt@gmail.com>
2025-11-09 23:44:56 -0500

    initial commit

---
+++ test/source/AudioProcessorTest.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <ArtOfMixingVisualizer/PluginProcessor.h>
+
+namespace audio_plugin_test {
+TEST(ArtOfMixingVisualizerAudioProcessor, Foo) {
+    ArtOfMixingVisualizerAudioProcessor processor{};
+    ASSERT_FALSE(true);
+}
+}
